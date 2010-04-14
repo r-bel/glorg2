332877565f71bfe112432bf386f7d08ad12f3c70
Henning Moe <Henning Moe@localhost>
2010-04-14 14:50:54 +0200

    Added files (I don't have VisualHg for VS2010 :( )

---
+++ Glorg2/Glorg2/Graphics/OpenGL/Primitives/Primitive.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Glorg2.Graphics.OpenGL.Primitives
+{
+	public abstract class Primitive
+	{
+		protected internal VertexBuffer<Vector3> vb;
+		protected internal IndexBuffer<ushort> ib;
+
+		protected abstract void Build();
+		public abstract DrawMode DrawMode { get; }
+
+		
+	}
+}
---
+++ Glorg2/Glorg2/Resource/Model3dsImporter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Glorg2.Resource
+{
+	public class Model3dsImporter : ResourceImporter
+	{
+		public override string FileDescriptor
+		{
+			get { return "model.3ds"; }
+		}
+
+		public override T Import<T>(System.IO.Stream source, string source_name, ResourceManager man)
+		{
+			return default(T);
+		}
+	}
+}
